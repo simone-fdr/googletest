ecb21d085c57ea3c285f98f76643cc5f9decf96b
Simone Foderà <57895129+simone-fdr@users.noreply.github.com>
2023-05-31 12:51:02 +0200

    Update compilemain.sh

--- googletest/samples/compilemain.sh
+++ googletest/samples/compilemain.sh
@@ -1,1 +1,1 @@
-g++ -Wextra -Wall googletest/samples/main_tests.cpp -o googletest/samples/target/main sample1.cc
+g++ -Wextra -Wall googletest/samples/main_tests.cpp -o googletest/samples/target/main googletest/samples/sample1.cc
