1a9ba66ff8df6d2e2b6fb7d505300f90a8dbab4b
Simone Foderà <57895129+simone-fdr@users.noreply.github.com>
2023-05-31 12:02:54 +0200

    Update compile.sh

--- googletest/samples/compile.sh
+++ googletest/samples/compile.sh
@@ -1,1 +1,1 @@
-gcc -Wall -Wextra -o target/sample1 googletest/samples/sample1.cc && gcc -Wall -Wextra -o target/sample2 googletest/samples/sample2.cc    
+gcc -Wall -Wextra -o googletest/samples/target/sample1 googletest/samples/sample1.cc && gcc -Wall -Wextra -o googletest/samples/target/sample2 googletest/samples/sample2.cc    
