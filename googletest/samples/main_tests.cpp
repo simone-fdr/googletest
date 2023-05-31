dabf25973add8e5fe6f0f0b0ca88c4ed688385a7
Maser-GitHub <simone000161@gmail.com>
2023-05-31 15:46:34 +0200

    Fixed errors

--- googletest/samples/main_tests.cpp
+++ googletest/samples/main_tests.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include "sample1.h"
-
+#include "gtest/gtest.h"
 using namespace std;
 
 int main()
@@ -10,27 +10,37 @@ int main()
 
     // read from file
     // name of file : input.txt
-    ifstream newfile;
-    newfile.open('input.txt');
+    //ifstream newfile;
+    //newfile.open('input.txt');
     // read contents here
-    newfile.close();
+    //newfile.close();
+
+
 
     // perform computations
     int x = 6;
-    int res = Factorial(x);
-    std::cout << "factorial of the number : " << std::endl;
-    std::cout << res << std::endl;
 
-    std::cout << "the number is prime?" << std::endl;
-    std::cout << IsPrime(x) << std::endl;
+    std::cout << "Input number: " << std::endl << x << std::endl;
+
+    int res = Factorial(x);
+    std::cout << "Factorial of the number: " << std::endl << res << std::endl;
+
+    bool isPrime = IsPrime(x);
+    std::cout << "Is the number prime?" << std::endl << isPrime << std::endl;
 
     // write to file
     // name of file : output.txt
-    ofstream output_file;
-    output_file.open('output.txt');
-    output_file << "results:" << std::endl;
+    //ofstream output_file;
+    //output_file.open('output.txt');
+    //output_file << "results:" << std::endl;
     // write reseults here
-    output_file.close();
+    //output_file.close();
+
+    TEST(FactorialTest, Negative) {
+        EXPECT_EQ(1, Factorial(-5));
+        EXPECT_EQ(1, Factorial(-1));
+        EXPECT_GT(Factorial(-10), 0);
+    }
 
     return 0;
 }
